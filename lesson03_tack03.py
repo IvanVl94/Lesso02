1875242886bdfcbba9a2f06db0e9de0c8bdb8630
IvanVl94 <123305766+IvanVl94@users.noreply.github.com>
2025-01-29 15:00:30 +0300

    Update lesson03_tack03.py

--- lesson03_tack03.py
+++ lesson03_tack03.py
@@ -1,8 +1,8 @@
-from adress import Adress
+from Address import Address
 from mailing import Mailing
 
-to_address = Adress(" 8888222", " Воркута",  " Сизам", " 21",  " 4")
-from_address = Adress(" 2223232", " Киркуду", " Волшебная", " 12", " 4")
+to_address = Address(" 8888222", " Воркута",  " Сизам", " 21",  " 4")
+from_address = Address(" 2223232", " Киркуду", " Волшебная", " 12", " 4")
 
 
 mail = Mailing(to_address, from_address, 2770, "HYD567")
